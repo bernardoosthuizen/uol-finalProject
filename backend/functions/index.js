03650df81c09d07019038727b4a337f723bcf8e6
Bernard Oosthuizen <bernard.oosthuizen@gmail.com>
2024-06-28 19:42:58 +0800

    rate limiting implemented

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -10,6 +10,7 @@ const bodyParser = require("body-parser");
 const { initializeApp } = require("firebase-admin/app");
 const { userValidator, idValidator, taskValidator } = require("./validators");
 const neo4j = require("neo4j-driver");
+const { rateLimit } = require("express-rate-limit");
 
 require("dotenv").config();
 
@@ -42,6 +43,18 @@ const app = express();
 // Adding middleware to parse request body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Set up rate limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 
 // -------------------------------------------------------
